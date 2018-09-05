66feb42f898d154c576bc73ee38c8101fc192f89
Vojtech Spiwok <spiwokv@vscht.cz>
2018-09-05 21:41:27 +0200

    good prototype

--- R/reweight.R
+++ R/reweight.R
@@ -1,43 +1,39 @@
 library(metadynminer)
-hillsf<-read.hills("HILLS", per=c(T,T))
-colvar<-read.table("COLVAR")
+hillsf<-read.hills("../data/HILLS", per=c(T,T))
+colvar<-read.table("../data/COLVAR")
 nfes <- 100
-temp <- 2.49*1000/8.314
+nbins <- 60
+temp <- 300
 gamma <- 10
 ebtac <- c()
 framestosum <- hillsf$size[1]/nfes
-tfes <- fes(hillsf, imax=1)-fes(hillsf, imax=1)
+tfes <- fes(hillsf, imax=1, npoints=nbins)-fes(hillsf, imax=1, npoints=nbins)
 for(i in 1:nfes) {
-  tfes <- tfes + fes(hillsf, imin=(i-1)*framestosum+1, imax=i*framestosum)
+  tfes <- tfes + fes(hillsf, imin=(i-1)*framestosum+1, imax=i*framestosum, npoints=nbins)
   s1 <- sum(exp(-1000*tfes$fes/8.314/temp))
   s2 <- sum(exp(-1000*tfes$fes/8.314/temp/gamma))
   ebtac<-c(ebtac,s1/s2)
 }
-cvsandbp <- data.frame(colvar[,2:4])
+icv1 <- ceiling((colvar[,2]+pi)*nbins/2/pi)
+icv2 <- ceiling((colvar[,3]+pi)*nbins/2/pi)
+bp <- colvar[,4]
+ebtacc <- rep(ebtac, each=nfes)
+if(length(ebtacc)>nrow(colvar)) ebtacc<-ebtacc[1:nrow(colvar)]
+if(length(ebtacc)<nrow(colvar)) ebtacc[length(ebtacc):nrow(colvar)]<-ebtacc[length(ebtacc)]
 
-#feshist<-function(x) {
-#  omat <- matrix(rep(0, 60*60), nrow=60)
-#  icv1 <- (x[1,1]+pi)*30/pi
-#  icv2 <- (x[1,2]+pi)*30/pi
-#  omat[icv1,icv2] <- exp(x[1,3]/8.314/0.3)
-#  return(omat)
-#}
-#fes <- matrix(rep(0, 60*60), nrow=60)
-#mlist <- list()
-#for(i in 1:nrow(cvsandbp)) {
-#  mlist <- list()
-#  mlist[[1]] <- fes
-#  mlist[[2]] <- feshist(cvsandbp[i,])
-#  fes <- Reduce('+', mlist)
-#}
+fes <- matrix(rep(0, nbins*nbins), nrow=nbins)
+for(i in 1:nbins) {
+  for(j in 1:nbins) {
+    fes[i,j]<-sum((icv1==i)*(icv2==j)*exp(1000*bp/8.314/temp)/ebtacc)
+  }
+}
 
-for(i in 1:60) {
-  for(j in 1:60) {
-    
-
-fes <- -8.314*0.3*log(fes)
+fes <- -8.314*temp*log(fes)/1000
 fes <- fes - min(fes)
-image(fes)
-
-
+fes[fes==Inf]<-100
+png("fes.png")
+tfes <- fes(hillsf, npoints=nbins)
+tfes$fes <- fes
+plot(tfes)
+dev.off()
 
