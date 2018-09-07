a9d2d842b626264a5155767d6e442838d2827ba2
Vojtech Spiwok <spiwokv@vscht.cz>
2018-09-07 09:44:35 +0200

    Update reweight.R

--- R/reweight.R
+++ R/reweight.R
@@ -4,8 +4,8 @@ library(metadynminer)
 #                  imin=1, imax=NULL, xlim=NULL, ylim=NULL)
 #reweightbonomi(cvfile, npoints=60, maxfe=100, temp=300, eunits="kJ/mol",
 #               imin=1, imax=NULL, xlim=NULL, ylim=NULL)
-#reweightiwary(cvfile, hillsfile, npoints=60, maxfe=100, nfes=100, temp=300,
-#              gamma=10, eunits="kJ/mol", imin=1, imax=NULL, xlim=NULL, ylim=NULL, usefes2=F)
+#reweighttiwary(cvfile, hillsfile, npoints=60, maxfe=100, nfes=100, temp=300,
+#               gamma=10, eunits="kJ/mol", imin=1, imax=NULL, xlim=NULL, ylim=NULL, usefes2=F)
 
 hillsf<-read.hills("../data/HILLS", per=c(T,T))
 colvar<-read.table("../data/COLVAR")
